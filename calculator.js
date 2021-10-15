a8d8ac6536ce2d95738e48fe465e285422a20657
Doğukan Çalışkan <dogukan.engt@gmail.com>
2021-10-15 13:55:01 +0300

    renewed functions

--- calculator.js
+++ calculator.js
@@ -1,75 +1,27 @@
-let inpvalue = document.querySelector(".inpvalue");
+var inpValue = document.querySelector(".inpvalue");
+let buttons = document.querySelectorAll("button");
 
-let functopr = () => {
+for (let i = 0; i < buttons.length; i++) {
 
-    if (inpvalue.value == "") {
-
-        inpvalue.value = "kidding?";
-        setTimeout(function() {
-            inpvalue.value = "";
-        }, 750)
-    };
-
-    inpvalue.value = eval(inpvalue.value);
-};
-
-
-function funct0() {
-    inpvalue.value += "0";
-};
-
-function funct1() {
-    inpvalue.value += "1";
-};
-
-function funct2() {
-    inpvalue.value += "2";
-};
-
-function funct3() {
-    inpvalue.value += "3";
-};
-
-function funct4() {
-    inpvalue.value += "4";
-};
-
-function funct5() {
-    inpvalue.value += "5";
-};
-
-function funct6() {
-    inpvalue.value += "6";
-};
-
-function funct7() {
-    inpvalue.value += "7";
-};
-
-function funct8() {
-    inpvalue.value += "8";
-};
-
-function funct9() {
-    inpvalue.value += "9";
-};
-
-function functC() {
-    inpvalue.value = "";
+    buttons[i].addEventListener("click", getTheButton);
 }
 
-function functpls() {
-    inpvalue.value += "+";
-};
+function getTheButton(button) {
+    let buttonText = button.target.textContent;
 
-function functmns() {
-    inpvalue.value += "-";
-};
+        
 
-function functcrs() {
-    inpvalue.value += "*";
-};
+    if (buttonText == "=") {
+        
+        inpValue.value = eval(inpValue.value);
+        inpValue.value = "";
+        buttonText = "";
+               
+    } else if (buttonText == "C") {
+        inpValue.value = "";
+        buttonText = "";
+    }
 
-function functdvd() {
-    inpvalue.value += "/";
-};
+    inpValue.value += buttonText;
+    button.preventDefault();
+}
