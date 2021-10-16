301225c34e9b32fb7f96fad2341fd94437aa4774
Doğukan Çalışkan <dogukan.engt@gmail.com>
2021-10-16 12:14:42 +0300

    functions for different background colors pick

--- calculator.js
+++ calculator.js
@@ -1,29 +1,47 @@
-var inpValue = document.querySelector(".inpvalue");
+let inpValue = document.querySelector(".inpvalue");
 let buttons = document.querySelectorAll("button");
+let colors = document.querySelectorAll(".calculator-colors");
+let calculatorSurface = document.querySelector(".calculator-surface");
 
+// statements for number and operator buttons
 for (let i = 0; i < buttons.length; i++) {
-
-    buttons[i].addEventListener("click", getTheButton);
+    buttons[i].addEventListener("click", getButton);
 }
 
-
-//calculator functions for each of them
-function getTheButton(button) {
+function getButton(button) {
     let buttonText = button.target.textContent;
 
-        
-
     if (buttonText == "=") {
-        
-        inpValue.value = eval(inpValue.value);
-        inpValue.value = "";
+        if (inpValue.value != "") {
+
+            inpValue.value = eval(inpValue.value);
+        }
         buttonText = "";
-               
+
     } else if (buttonText == "C") {
         inpValue.value = "";
         buttonText = "";
     }
 
     inpValue.value += buttonText;
-    button.preventDefault();
+}
+
+
+// statements for color selection for background
+for (let i = 0; i < colors.length; i++) {
+    colors[i].addEventListener("click", selectColor)
+}
+
+function selectColor(color) {
+
+    let selectedColor = color.target.style.backgroundColor;
+    calculatorSurface.style.backgroundColor = selectedColor;
+    inpValue.style.backgroundColor = "white";
+
+    let changeButtonColor = (buttonItem) => {
+        buttonItem.style.backgroundColor = "#ccffcc";
+        buttonItem.style.color = "black";
+    }
+    buttons.forEach(changeButtonColor);
+
 }
